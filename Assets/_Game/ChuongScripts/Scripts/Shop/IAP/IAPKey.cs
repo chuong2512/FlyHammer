e4fe1220d1d53fdfe96205d59b66583d51a628ee
chuong_gamer <hieutg244@gmail.com>
2024-05-06 21:47:35 +0700

    init game

---
+++ Assets/_Game/ChuongScripts/Scripts/Shop/IAP/IAPKey.cs
@@ -0,0 +1,11 @@
+namespace ChuongCustom
+{
+    public static class IAPKey {
+        public const string C_PACK1 = "hammer_goi_1";
+        public const string C_PACK2 = "hammer_goi_2";
+        public const string C_PACK3 = "hammer_goi_3";
+        public const string C_PACK4 = "hammer_goi_4";
+        public const string C_PACK5 = "hammer_goi_5";
+        public const string C_PACK6 = "hammer_goi_6";
+    }
+}
